d41891baf7e2396b57e214690e7b79bb74ac1238
sokomin <sokoranominsyu@gmail.com>
2022-01-04 01:39:53 +0900

    画像取得

--- exec.bat
+++ exec.bat
@@ -1,13 +1,11 @@
 @echo off
 git pull origin main
 
-python ranking_lv_jpn.py
-python ranking_gh_jpn.py
+python page__get.py
 
-git add out/jpn_lv/*.csv
-git add out/jpn_gh/*.csv
-git commit -m "[auto commit]daily japan ranking updated."
+git add out/origin_front/*.html
+git add out/translated/*.html
+git commit -m "[auto commit]daily korea information updated."
 
 rem "pushだけはすると事故る"
 rem "git push origin main"
-git push origin main
